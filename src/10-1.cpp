44f5df290b7145d8d4bdaa6535817e604defafa9
ren517 <2993159615@qq.com>
2026-05-19 14:30:34 +0800

    submit

--- src/10-1.cpp
+++ src/10-1.cpp
@@ -1,17 +1,18 @@
-class Trivial
-{
-	int i;
-public:
-	Trivial(int n) : i(n), j(n) {}
-	Trivial() {}
-	int j;
+#include <iostream>
+class Trivial {
+    int i;
+
+  public:
+    Trivial(int n) : i(n), j(n) {
+    }
+    Trivial() {
+    }
+    int j;
 };
 
-int main()
-{
-	Trivial a(5);
-	Trivial b;
-	b = a;
-	std::cout << "std::is_trivial_v<Trivial>   : " 
-<< std::is_trivial_v<Trivial> << std::endl;
+int main() {
+    Trivial a(5);
+    Trivial b;
+    b = a;
+    std::cout << "std::is_trivial_v<Trivial>   : " << std::is_trivial_v<Trivial> << std::endl;
 }
